4e3d8a0b7f0679b61f7577192639ab8f324454ee
Flagoul <basile.vu@gmail.com>
2015-11-05 01:17:47 +0100

    withinviweport

--- static/js/result.js
+++ static/js/result.js
@@ -1,8 +1,20 @@
-// Loads image urls when the div containing images appear
+
+var imgPath = "/api/images/";
+var defaultImgPath = imgPath + "default.png";
+
 $(document).ready(function () {
-    $('#cards > div').on('appear', function (e) {
-        $.get('images/' + e.attr('id'), function(res) {
-            e.children('img').attr('src', res);
+    $(window).scroll(loadImagesInViewport); // sets the action to take when scrolling
+    loadImagesInViewport(); // fetches the urls for the images on screen at page loading
+});
+
+// For each div of #cards visible on screen:
+// sets default image to tag 'img' and  fetches the url of the real image based on the 'div' id.
+// Once the url is fetched, sets the'src' attribute to the 'img' tag.
+function loadImagesInViewport() {
+    $("#cards > div").withinviewport().each(function () {
+        $(this).children('img').attr('src', defaultImgPath);
+        $.get(imgPath + $(this).attr('id'), function(res) {
+            $(this).children('img').attr('src', res);
         });
     });
-});
+}
