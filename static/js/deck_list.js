8ce083995b269588ce7a08fe6cfff3fb05279fb2
Flagoul <basile.vu@gmail.com>
2016-01-11 23:24:00 +0100

    Click on deck row

--- static/js/deck_list.js
+++ static/js/deck_list.js
@@ -1,20 +1,20 @@
 var jsonPath = "api/decks/json";
 
 $(document).ready(function () {
-   fetchJson(createDeckList);
+    fetchJson(createDeckList);
+    bindRows();
 });
 
 function fetchJson(callback) {
     var testJson = [
-        {name: "Test", colors: ["{R}", "{G}", "{B}"], cards: 42, side: 14},
-        {name: "Test2", colors: ["{R}", "{G}", "{W}"], cards: 40, side: 15}
+        {id: 42, name: "Test", colors: ["{R}", "{G}", "{B}"], cards: 42, side: 14},
+        {id: 1, name: "Test2", colors: ["{R}", "{G}", "{W}"], cards: 40, side: 15}
     ]
     callback(testJson);
     //$.get(jsonPath, callback(data));
 }
 
 function createDeckList(json) {
-
     var deckList = $('#deck-list > tbody');
 
     $.each(json, function (i, deck) {
@@ -22,13 +22,20 @@ function createDeckList(json) {
         console.log(deck);
 
         deckList.append(
-            '<tr>' +
-                '<th scope="row">' + i + '</th>' +
+            '<tr id="' + deck.id + '" class="deck-values">' +
+                '<th scope="row">' + (i+1) + '</th>' +
                 '<td>' + deck.name + '</td>' +
                 '<td>' + insertImagesInText(deck.colors.toString()) + '</td>' +
                 '<td>' + deck.cards + '</td>' +
                 '<td>' + deck.side + '/15</td>' +
             '</tr>'
         );
+    });
+}
+
+function bindRows() {
+    $('.deck-values').click(function () {
+        //console.log($(this).attr('id'));
+        document.location = "decks/" + $(this).attr('id');
     });
 }
