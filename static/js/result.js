91eae55a91bc081cf3f7e8131f401c2369835619
Flagoul <basile.vu@gmail.com>
2016-01-14 00:01:36 +0100

    Adding modal for card insertion in deck

--- static/js/result.js
+++ static/js/result.js
@@ -3,6 +3,13 @@ var imgPath = "/api/images/";
 var detailsPath = "/api/cards/";
 var defaultImgPath = imgPath + "default.png";
 var numCardPostPath = "/api/collection/";
+var deckPostPath = "api/decks/";
+
+// TODO change once api/decks ok
+var existingDecks = [
+    {id: 31, name: "Test", user_index: 1, colors:[], n_cards: 42, n_side: 4},
+    {id: 22, name: "Deck temp", user_index: 2, colors: [], n_cards: 33, n_side: 10}
+];
 
 // contains ids of elements which are currently in view port
 var loadedIds = [];
@@ -48,7 +55,28 @@ $(document).ready(function () {
     bindEditable();
 
     setupPost();
+
+    prepareDeckModal();
 });
+
+function prepareDeckModal() {
+    var options = '';
+    existingDecks.forEach(function (deck) {
+        options += '<option deck-id="' + deck.id + '">' + deck.name + '</option>';
+    });
+
+    $('#deck-selection').append(options);
+
+    // post data
+    $('#modal-submit-button').click(function () {
+        var deckId = $('#deck-selection').find(":selected").attr('deck-id');
+        var cardId = $('#modal-add-to-deck').attr('card-id');
+
+        $.post(deckPostPath + deckId, {card_id : cardId} , function (data) {
+            console.log(data);
+        });
+    });
+}
 
 
 // Resize divs heights at the right size and loads images
@@ -140,7 +168,6 @@ function createStrip(curDiv) {
     var innerDiv = curDiv.children('.card-inner');
 
     if (tot !== undefined && tot > 0) {
-        console.log('appending');
         innerDiv.append(
             '<div class="strip"></div>' +
             '<div class="strip-values">' + tot + '</div>'
@@ -203,8 +230,9 @@ function createDetails(id) {
     var cardDetails = $('#card-details');
 
     cardDetails.children('h2').text(details["name"]);
-    cardDetails.find('#number-cards-collection').empty();
+    cardDetails.find('#card-details-upper-right').empty();
     cardDetails.children('#card-details-lower').empty();
+    $('#modal-add-to-deck').attr('card-id', id);
 
     createDetailsUpper(id);
     createDetailsLower(details);
@@ -214,29 +242,43 @@ function createDetailsUpper(id) {
 
     var cardDiv = $('#' + id);
 
-    $('#number-cards-collection').append('<p>In collection</p>');
+    $('#card-details-upper-right').append('<p>In collection</p>');
     createRowNumCards(id, "Normal", cardDiv.attr('data-normal'));
     createRowNumCards(id, "Foil", cardDiv.attr('data-foil'));
+    createButtonAddToDeck();
 }
 
 function createRowNumCards(id, labelVal, num) {
     var row = $('<div class="row"></div>');
     var label = $('<label class="col-md-9">' + labelVal  + '</label>');
     var input = $('<a>' + num + '</a>').attr({
-        "class": "col-md-3",
-        "data-placement": "left",
-        "data-title": "Number of cards",
-        "data-type": "number",
-        "data-url": numCardPostPath + id,
-        "data-pk": id,
-        "data-name": labelVal
+        'class': 'col-md-3',
+        'data-placement': 'left',
+        'data-title': 'Number of cards',
+        'data-type': 'number',
+        'data-url': numCardPostPath + id,
+        'data-pk': id,
+        'data-name': labelVal
     });
 
     input.editable();
 
     row.append(label);
     row.append(input);
-    $('#number-cards-collection').append(row);
+    $('#card-details-upper-right').append(row);
+}
+
+function createButtonAddToDeck() {
+    var button = $('<button>Add to deck</button>');
+
+    button.attr({
+        'id': 'button-add-to-deck',
+        'class': 'btn btn-primary',
+        'data-toggle': 'modal',
+        'data-target': '#modal-add-to-deck'
+    });
+
+    $('#card-details-upper-right').append(button);
 }
 
 function createDetailsLower(details) {
@@ -303,22 +345,5 @@ function bindEditable() {
 
         url: numCardPostPath,
         name: "n_foil"
-    });
-}
-
-
-
-function addToCollection(id, nNormal, nFoil) {
-
-    setupPost();
-
-    var postData = {
-        normal : nNormal,
-        foil : nFoil
-    };
-    console.log(postData);
-
-    $.post("/api/collection/" + id, postData , function(data) {
-        console.log(data);
     });
 }
