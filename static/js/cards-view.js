3d49538c06b5717f44740e3aee18713a01d25d38
Flagoul <basile.vu@gmail.com>
2016-01-15 20:36:09 +0100

    Cleaning result.js

---
+++ static/js/cards-view.js
@@ -0,0 +1,287 @@
+/**
+ * This file contains functions to create view for the cards (loading into viewport, fetching card details, etc).
+ * The only thing which is needed when using this file is to define some functions to create the upper and lower
+ * parts of the card details and set them at the beginning with the setCreateDetailsFunctions function.
+ */
+
+// paths
+var imgPath = "/api/images/";
+var detailsPath = "/api/cards/";
+var defaultImgPath = imgPath + "default.png";
+
+// contains ids of elements which are currently in view port
+var loadedIds = [];
+
+// Contains info relative to image locked when image clicked.
+// 'locked' is if there is a locked image and 'lastId' is the id of the last card locked.
+var lockInfo = {
+    locked: false,
+    lastId: null
+};
+
+// Contains as keys the ids of the cards whose details are already fetched and as values the details as json.
+var detailsFetched = {};
+
+// functions to create details on the upper and lower parts of cards details.
+// Must be set with a call to setCreateDetailsFunctions before calling createCardDetails.
+var createDetailsUpperFunction, createDetailsLowerFunction;
+
+// Allows a callback to be called only when the scroll stops (better performances than just
+// calling the callback at each scroll, for example).
+// Scroll stop detection is based on a timer of 150ms long.
+$.fn.scrollStopped = function(callback) {
+    $(this).scroll(function (ev) {
+        clearTimeout($(this).data('scrollTimeout'));
+        $(this).data('scrollTimeout', setTimeout(callback.bind(this), 150, ev));
+    });
+};
+
+// Executed at loading
+$(document).ready(function () {
+
+    // if no result found
+    if ($('#cards').children().length == 0) {
+        $('#cards').append('<p id="no-result-text">No result found</p>');
+    }
+
+    else {
+
+        $(window).scrollStopped(loadImagesInViewport); // sets the action to take when scrolling stops
+        $(window).resize(initView);
+
+        setImagesErrorPath();
+        initView();
+
+        // what to do when clicking on card image
+        $('#cards > div').click(lockCardDetails);
+
+        // what to do when hovering on card image
+        $('#cards img').hover(function () {
+            if (!lockInfo.locked) {
+                displayCardDetails($(this).parent().parent().attr('id'));
+            }
+        });
+    }
+});
+
+// Applies the following behaviour on all cards: if image cannot load, displays default image.
+function setImagesErrorPath() {
+    $('img').attr('onerror', 'this.src="' + defaultImgPath + '"');
+}
+
+
+// Resizes divs heights at the right size and loads images
+function initView() {
+    resizeDivHeights();
+    setPreloadMargin($(window).height());
+    loadImagesInViewport();
+    setPreloadMargin(3 * $(window).height());
+}
+
+
+// Allows loading of cards in a margin of nPixels before the top and after the bottom of viewport.
+function setPreloadMargin(nPixels) {
+    withinviewport.defaults.top = -nPixels;
+    withinviewport.defaults.bottom = -nPixels;
+}
+
+
+// Resizes all div to image size.
+function resizeDivHeights() {
+    $('#cards > div').height(findImageHeight());
+}
+
+// Finds the current height of a card.
+function findImageHeight() {
+    return $('#cards > div').first().width() * 310 / 223; // 310 : card height, 223 : card width
+}
+
+// Unloads each div of #cards not in viewport.
+// For those which are :
+// sets default image to tag 'img' and loads image based on the 'div' id.
+// Once the image is loaded, sets the 'src' attribute to the image path.
+function loadImagesInViewport() {
+
+    $('#cards > div').withinviewport().each(function () {
+        var curDiv = $(this);
+        var curId = curDiv.attr('id');
+        var curImg = curDiv.find('img');
+
+        if (!curDiv.hasClass('loaded')) {
+
+            curDiv.addClass('loaded');
+            loadedIds.push(curDiv.attr('id'));
+
+            curImg.attr('src', defaultImgPath);
+            createStrip(curDiv);
+
+            // Requests loading of the image
+            $.ajax(imgPath + curId)
+
+                // image loaded
+                .done(function () {
+                    curImg.attr('src', imgPath + curId);
+                })
+
+                // image failed to load
+                .fail(function () {
+                    console.log("failed to load image number " + curId);
+                });
+        }
+
+    });
+
+    unloadNotInViewPort();
+}
+
+// "Unloads" the cards which are not in view port anymore by changing the 'src' attr of the img tag to "".
+function unloadNotInViewPort() {
+
+    for (var i = loadedIds.length-1; i >= 0; i--) {
+        var id = loadedIds[i];
+        var curDiv = $('#' + id);
+
+        if (!curDiv.is(':within-viewport')) {
+            curDiv.removeClass('loaded');
+            curDiv.find('img').attr('src', '');
+
+            loadedIds.splice(i, 1);
+        }
+    }
+
+}
+
+// Creates a strip on the current card.
+// 'curDiv' is the node which is the current div of the card.
+function createStrip(curDiv) {
+
+    var nNormal = parseInt(curDiv.attr('data-normal'));
+    var nFoil = parseInt(curDiv.attr('data-foil'));
+    var tot = nNormal + nFoil;
+
+    // if already created, update number
+    if (curDiv.find('.strip').length > 0) {
+        curDiv.find('.strip-values').text(tot);
+        return;
+    }
+
+    var innerDiv = curDiv.children('.card-inner');
+
+    if (tot !== undefined && tot > 0) {
+        innerDiv.append(
+            '<div class="strip"></div>' +
+            '<div class="strip-values">' + tot + '</div>'
+        );
+    }
+}
+
+// Locks the details displayed on the card clicked (so hovering on images does not display details of other cards).
+// If a card was already locked and the one clicked at this moment is the same, unlock it, else lock the current one.
+function lockCardDetails() {
+
+    var curId = $(this).attr('id');
+
+    // lock if no card is clicked
+    if (!lockInfo.locked) {
+        lockInfo.locked = true;
+        lockInfo.lastId = curId;
+    } else {
+
+        // unlock if click on same card, otherwise display the current card details
+        if (lockInfo.lastId === curId) {
+            lockInfo.locked = false;
+        } else {
+            lockInfo.lastId = curId;
+            displayCardDetails(curId);
+        }
+    }
+}
+
+// Opens the details panel of the card clicked.
+function displayCardDetails(id) {
+    $('#card-details-img').attr('src', imgPath + id);
+    fetchCardDetails(id);
+}
+
+// If the details for the card whose id is 'id' are not yet fetched, fetches them.
+// When the details are available, call the callback.
+function fetchCardDetails(id) {
+
+    if (detailsFetched[id] === undefined) {
+
+        // loading
+        var cardDetails = $('#card-details');
+        cardDetails.children('h2').text("Loading...");
+        cardDetails.children('#card-details-fields').text("Loading...");
+
+        // fetch details
+        $.get(detailsPath + id, function (data) {
+            detailsFetched[id] = data;
+            createDetails(id);
+        });
+    } else {
+        createDetails(id);
+    }
+}
+
+// Function to call at the beginning to define how to create the card details.
+function setCreateDetailsFunctions(createDetailsUpper, createDetailsLower) {
+    createDetailsUpperFunction = createDetailsUpper;
+    createDetailsLowerFunction = createDetailsLower;
+}
+
+// Creates the details view for the current locked card / hovered card.
+// The functions createDetailsUpper and createDetailsLower should be set beforehand with
+// the function setCreateDetailsFunctions.
+function createDetails(id) {
+
+    var details = detailsFetched[id];
+    var cardDetails = $('#card-details');
+
+    cardDetails.children('h2').text(details["name"]);
+    cardDetails.find('#card-details-upper-right').empty();
+    cardDetails.children('#card-details-lower').empty();
+
+    createDetailsUpperFunction(id);
+    createDetailsLowerFunction(id);
+}
+
+// Create a string from a value (in json). Placeholders like {W} are replaced with the url to icons.
+function createStringFromValue(details, key) {
+    return insertImagesInText(("" + details[key]));
+}
+
+// Create a string from a value (in json) which is an array. The resulting string has the \n replaced by <br>.
+function createStringFromArrayValue(details, key) {
+    var res = "";
+    if (details[key].length > 0) {
+        res = details[key][0];
+        for (var i = 1; i < details[key].length; ++i) {
+            res += ", " + details[key][i];
+        }
+    }
+    return res;
+}
+
+/**
+ * Creates a field corresponding to a detail value.
+ *
+ * Params :
+ * parentDiv : the div to which attach the created element
+ * details : object containing values
+ * key : the key to access the value which will be displayed
+ * name : value of the label
+ * createStringFunction : function which will create a string from the value. Useful to define behaviour related to the
+ * value type (for example arrays).
+ */
+function createDetailsField(parentDiv, details, key, name, createStringFunction) {
+    // if value at 'key' is set and a key 'key' exists, create the elements
+    if (details[key] !== null && details[key] !== undefined) {
+
+        var newDetail = $('<div class="row"></div>');
+        newDetail.append('<label class="col-md-6">' + name + '</label>');
+        newDetail.append('<div class="col-md-6">' + formatTextToHTMLContent(createStringFunction(details, key)) + '</div>');
+
+        parentDiv.append(newDetail);
+    }
+}
--- static/js/result.js
+++ static/js/result.js
@@ -1,20 +1,7 @@
 // paths
-var imgPath = "/api/images/";
-var detailsPath = "/api/cards/";
-var defaultImgPath = imgPath + "default.png";
 var numCardPostPath = "/api/collection/";
 var deckGetPath = "/api/decks";
 var deckPostPath = "/api/decks/";
-
-// contains ids of elements which are currently in view port
-var loadedIds = [];
-
-// Contains info relative to image locked when image clicked.
-// 'locked' is if there is a locked image and 'lastId' is the id of the last card locked.
-var lockInfo = {
-    locked: false,
-    lastId: null
-};
 
 // Contains as keys the ids of the cards whose details are already fetched and as values the details as json.
 var detailsFetched = {};
@@ -22,45 +9,11 @@ var detailsFetched = {};
 // existing decks, initialized at start
 var existingDecks;
 
-// Allows a callback to be called only when the scroll stops (better performances than just
-// calling the callback at each scroll, for example).
-// Scroll stop detection is based on a timer of 150ms long.
-$.fn.scrollStopped = function(callback) {
-    $(this).scroll(function (ev) {
-        clearTimeout($(this).data('scrollTimeout'));
-        $(this).data('scrollTimeout', setTimeout(callback.bind(this), 150, ev));
-    });
-};
-
 // Executed at loading
 $(document).ready(function () {
-
-    // if no result found
-    if ($('#cards').children().length == 0) {
-        $('#cards').append('<p id="no-result-text">No result found</p>');
-    }
-
-    else {
-
-        $(window).scrollStopped(loadImagesInViewport); // sets the action to take when scrolling stops
-        $(window).resize(initView);
-
-        fetchExistingDecks();
-        setImagesErrorPath();
-        initView();
-
-        // what to do when clicking on card image
-        $('#cards > div').click(lockCardDetails);
-
-        // what to do when hovering on card image
-        $('#cards img').hover(function () {
-            if (!lockInfo.locked) {
-                displayCardDetails($(this).parent().parent().attr('id'));
-            }
-        });
-
-        setupPost();
-    }
+    setupPost();
+    fetchExistingDecks();
+    setCreateDetailsFunctions(createDetailsUpper, createDetailsLower);
 });
 
 function fetchExistingDecks() {
@@ -69,191 +22,18 @@ function fetchExistingDecks() {
     });
 }
 
-function setImagesErrorPath() {
-    $('img').attr('onerror', 'this.src="' + defaultImgPath + '"');
-}
-
-
-// Resize divs heights at the right size and loads images
-function initView() {
-    resizeDivHeights();
-    setPreloadMargin($(window).height());
-    loadImagesInViewport();
-    setPreloadMargin(3 * $(window).height());
-}
-
-
-// Allows loading of cards in a margin of nPixels before the top and after the bottom of viewport.
-function setPreloadMargin(nPixels) {
-    withinviewport.defaults.top = -nPixels;
-    withinviewport.defaults.bottom = -nPixels;
-}
-
-
-// Resizes all div to image size.
-function resizeDivHeights() {
-    $('#cards > div').height(findImageHeight());
-}
-
-// Finds the current height of a card.
-function findImageHeight() {
-    return $('#cards > div').first().width() * 310 / 223; // 310 : card height, 223 : card width
-}
-
-// Unloads each div of #cards not in viewport.
-// For those which are :
-// sets default image to tag 'img' and loads image based on the 'div' id.
-// Once the image is loaded, sets the 'src' attribute to the image path.
-function loadImagesInViewport() {
-
-    $('#cards > div').withinviewport().each(function () {
-        var curDiv = $(this);
-        var curId = curDiv.attr('id');
-        var curImg = curDiv.find('img');
-
-        if (!curDiv.hasClass('loaded')) {
-
-            curDiv.addClass('loaded');
-            loadedIds.push(curDiv.attr('id'));
-
-            curImg.attr('src', defaultImgPath);
-            createStrip(curDiv);
-
-            // Requests loading of the image
-            $.ajax(imgPath + curId)
-
-                // image loaded
-                .done(function () {
-                    curImg.attr('src', imgPath + curId);
-                })
-
-                // image failed to load
-                .fail(function () {
-                    console.log("failed to load image number " + curId);
-                });
-        }
-
-    });
-
-    unloadNotInViewPort();
-}
-
-// "Unloads" the cards which are not in view port anymore by changing the 'src' attr of the img tag to "".
-function unloadNotInViewPort() {
-
-    for (var i = loadedIds.length-1; i >= 0; i--) {
-        var id = loadedIds[i];
-        var curDiv = $('#' + id);
-
-        if (!curDiv.is(':within-viewport')) {
-            curDiv.removeClass('loaded');
-            curDiv.find('img').attr('src', '');
-
-            loadedIds.splice(i, 1);
-        }
-    }
-
-}
-
-function createStrip(curDiv) {
-
-    var nNormal = parseInt(curDiv.attr('data-normal'));
-    var nFoil = parseInt(curDiv.attr('data-foil'));
-    var tot = nNormal + nFoil;
-
-    // if already created, update number
-    if (curDiv.find('.strip').length > 0) {
-        curDiv.find('.strip-values').text(tot);
-        return;
-    }
-
-    var innerDiv = curDiv.children('.card-inner');
-
-    if (tot !== undefined && tot > 0) {
-        innerDiv.append(
-            '<div class="strip"></div>' +
-            '<div class="strip-values">' + tot + '</div>'
-        );
-    }
-}
-
-// Locks the details displayed on the card clicked (so hovering on images does not display details of other cards).
-// If a card was already locked and the one clicked at this moment is the same, unlock it, else lock the current one.
-function lockCardDetails() {
-
-    var curId = $(this).attr('id');
-
-    // lock if no card is clicked
-    if (!lockInfo.locked) {
-        lockInfo.locked = true;
-        lockInfo.lastId = curId;
-    } else {
-
-        // unlock if click on same card, otherwise display the current card details
-        if (lockInfo.lastId === curId) {
-            lockInfo.locked = false;
-        } else {
-            lockInfo.lastId = curId;
-            displayCardDetails(curId);
-        }
-    }
-}
-
-// Opens the details panel of the card clicked.
-function displayCardDetails(id) {
-    $('#card-details-img').attr('src', imgPath + id);
-    fetchCardDetails(id);
-}
-
-// If the details for the card whose id is 'id' are not yet fetched, fetches them.
-// When the details are available, call the callback.
-function fetchCardDetails(id) {
-
-    if (detailsFetched[id] === undefined) {
-
-        // loading
-        var cardDetails = $('#card-details');
-        cardDetails.children('h2').text("Loading...");
-        cardDetails.children('#card-details-fields').text("Loading...");
-
-        // fetch details
-        $.get(detailsPath + id, function (data) {
-            detailsFetched[id] = data;
-            createDetails(id);
-        });
-    } else {
-        createDetails(id);
-    }
-}
-
-// Creates the details view for the current locked card / hovered card
-function createDetails(id) {
-
-    var details = detailsFetched[id];
-    var cardDetails = $('#card-details');
-
-    cardDetails.children('h2').text(details["name"]);
-    cardDetails.find('#card-details-upper-right').empty();
-    cardDetails.children('#card-details-lower').empty();
-
-    createDetailsUpper(id);
-    createDetailsLower(details);
-}
-
 // Creates upper part of the card details
 function createDetailsUpper(id) {
 
+    var details = detailsFetched[id];
     var parentDiv = $('#card-details-upper-right');
-
-    // TODO: can be changed when cards as json
-    var cardDiv = $('#' + id);
+    var cardDiv = $('#' + id); // TODO: can be changed when cards as json
 
     parentDiv.append('<p>In collection</p>');
     createRowNumCards(parentDiv, id, "Normal", cardDiv.attr('data-normal'), true);
     createRowNumCards(parentDiv, id, "Foil", cardDiv.attr('data-foil'), false);
     createButtonAddToDeck(id, parentDiv);
 
-    var details = detailsFetched[id];
     if (details["types"][0] !== "Land") {
         createDetailsField(parentDiv, details, "manaCost", "Mana Cost", createStringFromValue);
         createDetailsField(parentDiv, details, "cmc", "Cmc", createStringFromValue);
@@ -264,8 +44,10 @@ function createDetailsUpper(id) {
 }
 
 // Creates lower parts of card details
-function createDetailsLower(details) {
+function createDetailsLower(id) {
+    var details = detailsFetched[id];
     var parentDiv = $('#card-details-lower');
+
     createDetailsField(parentDiv, details, "orig_text", "Card Text", createStringFromValue);
     createDetailsField(parentDiv, details, "flavor", "Flavor Text", createStringFromValue);
     createDetailsField(parentDiv, details, "edition", "Edition", createStringFromValue);
@@ -314,10 +96,7 @@ function createRowNumCards(parentDiv, id, labelVal, num, isNormal) {
                 n_foil: n_foil
             };
 
-            console.log("card number update send data : {normal:" + postData.n_normal + ", foil:" + postData.n_foil + "}");
-
             $.post(numCardPostPath, postData, function (data) {
-                console.log("card number update response data : {normal:" + data.normal + ", foil:" + data.foil + "}");
                 link.text(isNormal ? data.normal : data.foil);
 
                 var cardDiv = $('#' + id);
